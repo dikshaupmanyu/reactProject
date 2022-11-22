48c84b906c2060156a1dbbde2412af8ce57fa5ac
Meraz Ahmed <76992780+MerazAhmed24@users.noreply.github.com>
2022-11-22 16:32:02 +0530

    done

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // const DB = process.env.DATABASE
-const DB = "mongodb+srv://yash:12345@cluster0.wpwahzr.mongodb.net/mernstack?retryWrites=true&w=majority"
-
+// const DB = "mongodb+srv://yash:12345@cluster0.wpwahzr.mongodb.net/mernstack?retryWrites=true&w=majority"
+const DB = "mongodb://localhost:27017/react";
 mongoose.connect(DB,{
 }).then(()=>console.log("DATABASE connected")).catch((err)=> console.log("error" + err.message))
